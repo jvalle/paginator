1b8381feaf0ff2665d4a575d38a2e0d5599e971d
jvalle <jcvalle@mail.usf.edu>
2013-05-06 18:23:01 -0300

    Create paginator.js

---
+++ paginator.js
@@ -0,0 +1,29 @@
+// Paginator jQuery plugin
+// Written By: Jason Valle
+// No License Here, just have fun
+
+(function($) {
+  $.fn.paginator = function() {
+		var itemsToPaginate = [],
+			numToShow = parseInt($(this).attr('data-pagination')),
+			showCounter = 0;
+
+		itemsToPaginate = $(this).children();
+		itemsToPaginate.hide();
+
+		function showMore(list, num) {
+	        for (var i = showCounter; i < showCounter + num; i++) {
+	            list.eq(i).show();
+	        }   
+	        showCounter += num;
+	        list.eq(showCounter - 1).append('<p><a class=\"seeMore\" style=\"cursor: pointer;\">See More</a></p>');
+
+	        $('.seeMore').on('click', function() {
+	            this.remove();
+	            showMore(itemsToPaginate, num);
+	        });
+	    };
+
+	    showMore(itemsToPaginate, numToShow);
+	};
+})(jQuery);
