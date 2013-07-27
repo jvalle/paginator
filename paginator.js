510c2f6e99d1ca4ddfebe6d1e792adbd50228332
Jason <jcvalle@mail.usf.edu>
2013-07-27 14:23:59 -0400

    Added showAll as an option you can pass when calling paginator to provide a Show All button.

--- paginator.js
+++ paginator.js
@@ -10,14 +10,10 @@
         }, options);
 
         return this.each(function(index) {
-
-            // console.log(index);
-
+            
             var showCounter = 0,
             itemsToPaginate = $(this).children(),
             numToShow = parseInt($(this).attr('data-pagination'));
-
-            console.log(itemsToPaginate.length);
 
             if (itemsToPaginate.length > numToShow) {
                 itemsToPaginate.hide();
@@ -28,15 +24,16 @@
             function showMore(num) {
                 var $showMore = $('<a class="showMore" style="cursor: pointer">Show More</a>'),
                     $showAll = $('<a class="showAll" style="cursor: pointer">Show All</a>'),
-                    $p = $('<p></p>').append($showMore).append($showAll)
+                    $p = $('<p></p>').append($showMore);
 
+                    if (settings.showAll) {
+                        $p = $p.append($showAll);
+                    }
 
                 for (var i = showCounter; i < showCounter + num; i++) {
                     itemsToPaginate.eq(i).show();
                 }   
                 showCounter += num;
-
-                // console.log(showCounter);
 
                 itemsToPaginate.eq(showCounter - 1).append($p);
 
